3613899f84e70fcbb10e3b53b1b9638bea062859
Nathan Golba <ngolba@gmail.com>
2018-10-18 16:47:57 -0400

    synch schema

--- schema.sql
+++ schema.sql
@@ -14,3 +14,8 @@ use bamazon;
 -- );
 
 select * from bamazon.products;
+
+-- Mock Data -- 
+-- insert into products (product_name, department_name, price, stock_quantity) 
+-- values ('Playing Cards', 'Toys', 9.95, 5), ('T-Shirt', 'Clothing', 18.00, 3), ('Headphones', 'Electronics', 30.00, 15), ('Slow Cooker', 'Kitchen', 70, 4), ('Compression Socks', 'Clothing', 14.95, 8),
+--     ('Action Figure', 'Toys', 14.84, 2), ('Bartender Kit', 'Kitchen', 44.95, 1), ('Game Console', 'Electronics', 480.95, 10), ('DVD', 'Media', 23.85, 50), ('Sunglasses', 'Clothing', 29.99, 8)
